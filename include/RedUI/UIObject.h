c032c28afde320f3e2df0b39e5aabbccfa97f176
worse <oskar.rabo@gmail.com>
2026-04-16 17:13:14 +0200

    Create rough implementation for UIObject class

---
+++ include/RedUI/UIObject.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+#include "Color/RGB.h"
+#include "Math/Vec3.h"
+
+namespace RedUI
+{
+	// Base class for all ui components.
+	class	UIObject
+	{
+	protected:
+		UIObject				*Parent = nullptr;
+		std::vector<UIObject *>	Children = {};
+		Math::Vec3				WorldPosition = Math::Vec3();
+		Math::Vec3				WorldScale = Math::Vec3();
+
+	public:
+		bool		Enabled;
+		Math::Vec3	Position;
+		Math::Vec3	Scale;
+		Color::RGB	Color;
+		float		Alpha;
+
+		UIObject(Math::Vec3 position = Math::Vec3(), Math::Vec3 scale = Math::Vec3(1, 1, 1),
+			Color::RGB color = Color::RGB(), float alpha = 1.0f, UIObject *parent = nullptr);
+		// Set parent of object. Parent is root (unparented) if nullptr or no argument.
+		void	SetParent(UIObject *parent = nullptr);
+	};
+}
---
+++ src/RedUI/UIObject.cpp
@@ -0,0 +1,32 @@
+#include "RedUI/UIObject.h"
+
+#include <algorithm>
+
+#include "RedUI/UIState.h"
+
+using namespace RedUI;
+using namespace RedUI::Math;
+using namespace RedUI::Color;
+
+UIObject::UIObject(const Vec3 position, const Vec3 scale, const RGB color, const float alpha, UIObject *parent)
+{
+	Position = position;
+	Scale = scale;
+	Color = color;
+	Alpha = alpha;
+	SetParent(parent);
+}
+
+// If parent == nullptr, then parent is root.
+void UIObject::SetParent(UIObject *parent)
+{
+	if (UIState::IsUpdating)
+	{
+		UIState::QueuedHierarchyChanges[this] = parent;
+		return ;
+	}
+
+	// todo: add logic to remove from old parent and set to root if new parent = nullptr
+	Parent = parent;
+	Parent->Children.push_back(this);
+}
