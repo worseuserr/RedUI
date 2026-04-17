bdf71bd176ac79d6e6c7274dc713afe5f8247d63
worse <oskar.rabo@gmail.com>
2026-04-17 15:51:37 +0200

    Change implementation to properly use WorldPosition and WorldScale instead of Position and Scale

--- src/RedUI/Components/Box.cpp
+++ src/RedUI/Components/Box.cpp
@@ -5,7 +5,7 @@ using namespace RedUI;
 
 void Box::Draw()
 {
-	GRAPHICS::DRAW_RECT(Position.X, Position.Y, Scale.X, Scale.Y,
+	GRAPHICS::DRAW_RECT(WorldPosition.X, WorldPosition.Y, WorldScale.X, WorldScale.Y,
 		Color.R, Color.G, Color.B,
 		static_cast<int>(Alpha * 255),
 		true, true);
