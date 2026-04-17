4c6ab0bd547e8aefb0eddb501a8ed7a5f103cc96
worse <oskar.rabo@gmail.com>
2026-04-17 11:45:02 +0200

    Add Create function for instantiating ui components

--- include/RedUI.h
+++ include/RedUI.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "RedUI/UIObject.h"
+#include "RedUI/UIState.h"
 
 namespace RedUI
 {
@@ -9,6 +11,23 @@ namespace RedUI
 	bool	CheckVersion(unsigned int version);
 	// Shows an error message and returns false if RedUI version is below passed version.
 	bool	RequireVersion(unsigned int version);
-	// Updates all UI components. This must be called every tick.
+	// Updates all UI components. This must be called every frame.
 	void	Update();
+	// Creates a UI component on the heap and returns a non-owning raw pointer to it. If parent is nullptr, the component is root.
+	template <typename T, typename ...Args>
+	requires (std::derived_from<T, UIObject>)
+	T		*Create(UIObject *parent, Args&&... args)
+	{
+		T					*raw;
+		std::unique_ptr<T>	obj = std::make_unique<T>(std::forward<Args>(args)...);
+
+		raw = obj.get();
+		if (parent == nullptr)
+			UIState::RootObjects.push_back(std::move(obj));
+		else
+			parent->GetChildren().push_back(std::move(obj));
+		return (raw);
+	}
+	// Remove ui component from screen by reference.
+	void	Remove(UIObject *object);
 }
