c4743f1c763affc5fc43404e4e97e714d171f1d9
worse <oskar.rabo@gmail.com>
2026-04-17 15:51:56 +0200

    Add newline

--- src/RedUI/Components/Box.cpp
+++ src/RedUI/Components/Box.cpp
@@ -5,7 +5,8 @@ using namespace RedUI;
 
 void Box::Draw()
 {
-	GRAPHICS::DRAW_RECT(WorldPosition.X, WorldPosition.Y, WorldScale.X, WorldScale.Y,
+	GRAPHICS::DRAW_RECT(
+		WorldPosition.X, WorldPosition.Y, WorldScale.X, WorldScale.Y,
 		Color.R, Color.G, Color.B,
 		static_cast<int>(Alpha * 255),
 		true, true);
