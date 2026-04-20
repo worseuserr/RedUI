939ce0358276c69ceb7ad450c99db32bd37fd644
worse <oskar.rabo@gmail.com>
2026-04-20 12:30:34 +0200

    Add attack and aim input hashes

--- include/RedUI/Input/Mouse.h
+++ include/RedUI/Input/Mouse.h
@@ -8,6 +8,8 @@ namespace RedUI::Input
 {
 	constexpr Hash	INPUT_CURSOR_X = 0xD6C4ECDC;
 	constexpr Hash	INPUT_CURSOR_Y = 0xE4130778;
+	constexpr Hash	INPUT_AIM = 0xF84FA74F;
+	constexpr Hash	INPUT_ATTACK = 0x07CE1E61;
 
 	Math::Vec2		GetMousePosition();
 	bool			IsLeftMouseDown();
