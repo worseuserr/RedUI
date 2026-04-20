699a044b6f3cb3b83475799a3b6e89e78eb082b7
worse <oskar.rabo@gmail.com>
2026-04-20 17:23:02 +0200

    Convert GetMousePosition to use windows api due to shitty native

--- src/RedUI/Input/Mouse.cpp
+++ src/RedUI/Input/Mouse.cpp
@@ -1,12 +1,30 @@
+#include <windows.h>
 #include "RedUI/Input/Mouse.h"
-
 #include "RedUI/Input/Key.h"
 
 using namespace RedUI;
 
+// Uses windows api to get mouse position because the shitty ass native doesn't work while not in a menu.
+// This can probably be optimized. TODO: optimize
 Math::Vec2 Input::GetMousePosition()
 {
-	return (Math::Vec2(PAD::GET_CONTROL_NORMAL(0, INPUT_CURSOR_X), PAD::GET_CONTROL_NORMAL(0, INPUT_CURSOR_Y)));
+	POINT		cursorPos;
+	HWND		gameWindow;
+	int			windowWidth;
+	int			windowHeight;
+	RECT		clientRect;
+
+	gameWindow = FindWindowA("sgaWindow", "Red Dead Redemption 2");
+	if (gameWindow == nullptr)
+		return (Math::Vec2());
+	GetClientRect(gameWindow, &clientRect);
+	windowHeight = clientRect.bottom - clientRect.top;
+	windowWidth = clientRect.right - clientRect.left;
+	if (windowWidth == 0 || windowHeight == 0)
+		return (Math::Vec2());
+	GetCursorPos(&cursorPos);
+	ScreenToClient(gameWindow, &cursorPos);
+	return (Math::Vec2(static_cast<float>(cursorPos.x) / windowWidth, static_cast<float>(cursorPos.y) / windowHeight));
 }
 
 bool Input::IsLeftMouseDown()
