ab0397e631207a14e7aac76e73e6cca8c469e674
worse <oskar.rabo@gmail.com>
2026-04-20 12:59:19 +0200

    Fix missing definitions

--- src/RedUI/UIState.cpp
+++ src/RedUI/UIState.cpp
@@ -4,8 +4,10 @@ using namespace RedUI;
 
 bool								UIState::IsUpdating = false;
 bool								UIState::CursorEnabled = false;
+bool								UIState::MouseInputsDisabled = false;
 float								UIState::ScreenVerticalRatio = 1.0f;
 std::vector<UIObjectOwner>			UIState::RootObjects = {};
 std::vector<AnimationOwner>			UIState::Animations = {};
+std::vector<UIObject *>				UIState::QueuedObjectDeletions = {};
 std::map<UIObject *, UIObject *>	UIState::QueuedHierarchyChanges = {};
 std::vector<AnimationOwner *>		UIState::QueuedFinishedAnimations = {};
