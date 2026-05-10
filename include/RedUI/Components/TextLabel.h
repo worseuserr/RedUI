36986260fb1a1128cb3b6542c46db957118d649a
worse <oskar.rabo@gmail.com>
2026-05-11 00:53:15 +0200

    Implement TextLabel component fully

--- include/RedUI/Components/TextLabel.h
+++ include/RedUI/Components/TextLabel.h
@@ -1,17 +1,29 @@
 #pragma once
-#include "RedUI/UIObject.h"
+#include "RedUI/Components/Box.h"
 #include "RedUI/Color/Dropshadow.h"
 
 namespace RedUI
 {
-	class	TextLabel : public UIObject
+	class	TextLabel : public Box
 	{
 	public:
-		using				UIObject::UIObject;
 		Color::Dropshadow	Dropshadow;
 		float				TextScale;
+		Color::RGB			TextColor;
+		float				TextAlpha;
 		std::string			Text;
+		bool				TextCentered = false;
+		// bool				TextWrapping -> Could possibly be added in the future.
 
+		TextLabel(const std::string &text, const Math::Vec2 &position = {},
+			float textScale = 1.0f, const Color::RGB &textColor = {},
+			float textAlpha = 1.0f, const Math::Vec2 &backgroundScale = {1, 1},
+			const Color::RGB &backgroundColor = {}, float backgroundAlpha = 0.0f);
 		void	Draw() override;
+		void	AnimateTextScale(float startScale, float endScale, Time::Milliseconds duration, Easing easing = Easing::Linear);
+		void	AnimateTextColor(const Color::RGB &startColor, const Color::RGB &endColor, Time::Milliseconds duration, Easing easing = Easing::Linear);
+		void	AnimateTextAlpha(float startAlpha, float endAlpha, Time::Milliseconds duration, Easing easing = Easing::Linear);
+		// void	AnimateDropshadow() -> Maybe.
+		// void	AnimateText() -> Planned, with different animation styles (swap, erase then fill, glitch, fade in, etc).
 	};
 }
--- src/RedUI/Components/TextLabel.cpp
+++ src/RedUI/Components/TextLabel.cpp
@@ -1,11 +1,50 @@
 #include "RedUI/Components/TextLabel.h"
 #include "Sdk/natives.h"
 
-void RedUI::TextLabel::Draw()
+using namespace RedUI;
+using namespace RedUI::Math;
+using namespace RedUI::Color;
+
+TextLabel::TextLabel(const std::string &text, const Vec2 &position,
+			const float textScale, const RGB &textColor,
+			const float textAlpha, const Vec2 &backgroundScale,
+			const RGB &backgroundColor, const float backgroundAlpha)
 {
-	UI::SET_TEXT_SCALE(0, TextScale);
-	UI::SET_TEXT_COLOR_RGBA(Color.R, Color.G, Color.B, Alpha);
-	UI::SET_TEXT_CENTRE(false);
-	UI::SET_TEXT_DROPSHADOW(Dropshadow.Distance, Dropshadow.Color.R, Dropshadow.Color.G, Dropshadow.Color.B, Dropshadow.Alpha);
-	UI::DRAW_TEXT(MISC::CREATE_STRING(10, "LITERAL_STRING", const_cast<char *>(Text.c_str())), WorldPosition.X, WorldPosition.Y);
+	Text = text;
+	Position = position;
+	TextScale = textScale;
+	TextColor = textColor;
+	TextAlpha = textAlpha;
+	Scale = backgroundScale;
+	Color = backgroundColor;
+	Alpha = backgroundAlpha;
+	Dropshadow = {};
 }
+
+void TextLabel::Draw()
+{
+	const Vec2	pos = WorldPosition + RenderOffset;
+	const Vec2	scale = WorldScale * RenderScale;
+
+	Box::Draw();
+	UI::SET_TEXT_SCALE(0, TextScale);
+	UI::SET_TEXT_COLOR_RGBA(TextColor.R, TextColor.G, TextColor.B, TextAlpha * 255);
+	UI::SET_TEXT_CENTRE(TextCentered);
+	UI::SET_TEXT_DROPSHADOW(Dropshadow.Distance, Dropshadow.Color.R, Dropshadow.Color.G, Dropshadow.Color.B, Dropshadow.Alpha * 255);
+	UI::DRAW_TEXT(
+		MISC::CREATE_STRING(
+			10,
+			"LITERAL_STRING",
+			const_cast<char *>(Text.c_str())),
+		pos.X - scale.X * 0.5,
+		pos.Y - scale.Y * 0.5);
+}
+
+void TextLabel::AnimateTextScale(const float startScale, const float endScale, const Time::Milliseconds duration, const Easing easing)
+{ Animate<float>(&TextScale, startScale, endScale, duration, easing); }
+
+void TextLabel::AnimateTextColor(const RGB &startColor, const RGB &endColor, const Time::Milliseconds duration, const Easing easing)
+{ Animate<RGB>(&TextColor, startColor, endColor, duration, easing); }
+
+void TextLabel::AnimateTextAlpha(const float startAlpha, const float endAlpha, const Time::Milliseconds duration, const Easing easing)
+{ Animate<float>(&TextAlpha, startAlpha, endAlpha, duration, easing); }
