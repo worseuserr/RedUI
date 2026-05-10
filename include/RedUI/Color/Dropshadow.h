a658195492b9649acd5d22fb72ec46a31715c846
worse <oskar.rabo@gmail.com>
2026-05-11 00:04:37 +0200

    Add default Distance of 0

--- include/RedUI/Color/Dropshadow.h
+++ include/RedUI/Color/Dropshadow.h
@@ -10,7 +10,7 @@ namespace RedUI::Color
 		RGB				Color;
 		float			Alpha;
 
-		Dropshadow(unsigned char distance, RGB color = {0, 0, 0}, float alpha = 1.0f);
+		Dropshadow(unsigned char distance = 0, RGB color = {0, 0, 0}, float alpha = 1.0f);
 		bool			operator==(const Dropshadow &other) const;
 		Dropshadow		operator+(const Dropshadow &other) const;
 		Dropshadow		operator-(const Dropshadow &other) const;
