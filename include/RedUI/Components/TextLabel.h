c35c25a9fe6737fdfd15caed1ae93142b24e59cb
worse <oskar.rabo@gmail.com>
2026-06-03 14:54:22 +0200

    Fix visiblity of Draw in components

--- include/RedUI/Components/TextLabel.h
+++ include/RedUI/Components/TextLabel.h
@@ -6,6 +6,9 @@ namespace RedUI
 {
 	class	TextLabel : public Box
 	{
+	protected:
+		void	Draw() override;
+
 	public:
 		Color::Dropshadow	Dropshadow;
 		float				TextScale;
@@ -19,7 +22,6 @@ namespace RedUI
 			float textScale = 1.0f, const Color::RGB &textColor = {},
 			float textAlpha = 1.0f, const Math::Vec2 &backgroundScale = {1, 1},
 			const Color::RGB &backgroundColor = {}, float backgroundAlpha = 0.0f);
-		void	Draw() override;
 		void	AnimateTextScale(float startScale, float endScale, Time::Milliseconds duration, Easing easing = Easing::Linear);
 		void	AnimateTextColor(const Color::RGB &startColor, const Color::RGB &endColor, Time::Milliseconds duration, Easing easing = Easing::Linear);
 		void	AnimateTextAlpha(float startAlpha, float endAlpha, Time::Milliseconds duration, Easing easing = Easing::Linear);
