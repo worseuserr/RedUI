6ca4391357a9ffc6f159f2b1e703cfad3b617f3f
worse <oskar.rabo@gmail.com>
2026-08-25 23:24:17 +0200

    Fix ParentNewUIObject not changing the actual parent field

--- src/RedUI/Object/Create.cpp
+++ src/RedUI/Object/Create.cpp
@@ -5,5 +5,6 @@ using namespace RedUI::Object;
 
 void	RedUI::Object::ParentNewUIObject(UIObjectOwner obj, UIObject *newParent)
 {
+	obj->Parent = newParent;
 	(newParent == nullptr ? UIObject::RootObjects : newParent->GetChildren()).push_back(std::move(obj));
 }
