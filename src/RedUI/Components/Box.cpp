8a0134376cf8f0d9fa5348e6fbfa2ebcee65527b
worse <oskar.rabo@gmail.com>
2026-08-26 16:20:29 +0200

    Fix using sprite fields for draw rect and order of drawing

--- src/RedUI/Components/Box.cpp
+++ src/RedUI/Components/Box.cpp
@@ -9,20 +9,20 @@ void Box::Draw()
 	const Vec2	pos = WorldPosition + RenderOffset;
 	const Vec2	scale = WorldScale * RenderScale;
 
+	GRAPHICS::DRAW_RECT(
+		pos.X, pos.Y, scale.X, scale.Y,
+		Color.R, Color.G, Color.B,
+		static_cast<int>(Alpha * 255),
+		true, true);
 	if (Sprite != nullptr)
 		GRAPHICS::DRAW_SPRITE(
 			Sprite->TextureDict.c_str(),
 			Sprite->TextureName.c_str(),
 			pos.X, pos.Y, scale.X, scale.Y,
 			Sprite->Heading,
-			Color.R, Color.G, Color.B,
-			static_cast<int>(Alpha * 255),
+			SpriteColor.R, SpriteColor.G, SpriteColor.B,
+			static_cast<int>(SpriteAlpha * 255),
 			true);
-	GRAPHICS::DRAW_RECT(
-		pos.X, pos.Y, scale.X, scale.Y,
-		SpriteColor.R, SpriteColor.G, SpriteColor.B,
-		static_cast<int>(SpriteAlpha * 255),
-		true, true);
 }
 
 bool Box::ContainsPoint(const Vec2 &point)
