60949eab23c281800d5aa6d263841ecc14915296
worse <oskar.rabo@gmail.com>
2026-08-30 17:42:46 +0200

    Add File class and IO namespace

---
+++ include/RedUI/IO/File.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <fstream>
+#include <string>
+
+namespace RedUI::IO
+{
+	class	File
+	{
+		std::fstream	Stream;
+		std::string		Filename;
+
+	public:
+		File(const char *filename, bool replaceIfExists = false);
+
+		// Returns IsOpen().
+		bool        Open(std::ios_base::openmode mode = std::ios_base::app);
+		void        Close();
+		void        Write(const char *str);
+		std::string	Read() const;
+		bool        IsOpen() const;
+		static bool Exists(const char *filename);
+
+		template	<typename T>
+		File		&operator<<(const T& data)
+		{
+			Stream << data;
+			return (*this);
+		}
+		template	<typename T>
+		File		&operator>>(T& value)
+		{
+			Stream >> value;
+			return (*this);
+		}
+	};
+}
---
+++ src/RedUI/IO/File.cpp
@@ -0,0 +1,46 @@
+#include "RedUI/IO/File.h"
+#include <filesystem>
+
+using namespace RedUI;
+
+
+IO::File::File(const char *filename, const bool replaceIfExists)
+{
+	Filename = filename;
+	Stream.open(filename, std::ios_base::out | (replaceIfExists ? std::ios_base::trunc : std::ios_base::app));
+	Stream.close();
+}
+
+bool IO::File::Open(const std::ios_base::openmode mode)
+{
+	Stream.open(Filename, mode);
+	return (IsOpen());
+}
+
+void IO::File::Close()
+{
+	Stream.close();
+}
+
+void IO::File::Write(const char *str)
+{
+	Stream << str;
+}
+
+std::string IO::File::Read() const
+{
+	std::stringstream	buff;
+
+	buff << Stream.rdbuf();
+	return (std::string(buff.str()));
+}
+
+bool IO::File::IsOpen() const
+{
+	return (Stream.is_open());
+}
+
+bool IO::File::Exists(const char *filename)
+{
+	return (std::filesystem::exists(filename));
+}
