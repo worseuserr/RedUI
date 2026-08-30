9d9df04dc55bf5049514fc7c558b65506efb7292
worse <oskar.rabo@gmail.com>
2026-08-30 21:10:28 +0200

    Add GetCurrentTime() and move Time function implementions to cpp file

--- include/RedUI/Time/Time.h
+++ include/RedUI/Time/Time.h
@@ -8,25 +8,12 @@ namespace RedUI::Time
 	using	Nanoseconds = unsigned long long;
 	using	Seconds = unsigned long long;
 
-	inline Milliseconds	MsSinceEpoch()
-	{
-		return (duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count());
-	}
+	Milliseconds	MsSinceEpoch();
+	Microseconds	UsSinceEpoch();
+	Nanoseconds		NsSinceEpoch();
+	Seconds			SSinceEpoch();
 
-	inline Microseconds	UsSinceEpoch()
-	{
-		return (duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count());
-	}
-
-	inline Nanoseconds	NsSinceEpoch()
-	{
-		return (duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now().time_since_epoch()).count());
-	}
-
-	inline Seconds		SSinceEpoch()
-	{
-		return (duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now().time_since_epoch()).count());
-	}
+	tm	GetCurrentTime();
 
 	// TODO: add a method for delaying functions
 }
---
+++ src/RedUI/Time/Time.cpp
@@ -0,0 +1,34 @@
+#include "RedUI/Time/Time.h"
+
+using namespace RedUI;
+
+Time::Milliseconds Time::MsSinceEpoch()
+{
+	return (duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count());
+}
+
+Time::Microseconds Time::UsSinceEpoch()
+{
+	return (duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count());
+}
+
+Time::Nanoseconds Time::NsSinceEpoch()
+{
+	return (duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now().time_since_epoch()).count());
+}
+
+Time::Seconds Time::SSinceEpoch()
+{
+	return (duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now().time_since_epoch()).count());
+}
+
+tm Time::GetCurrentTime()
+{
+	time_t				now;
+	tm					current;
+
+	now = time(nullptr);
+	if (localtime_s(&current, &now) != 0)
+		throw (std::exception());
+	return (current);
+}
