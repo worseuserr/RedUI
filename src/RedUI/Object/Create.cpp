b15d7ea1004bb00d29d3ba924a70dc880c6adf60
worse <oskar.rabo@gmail.com>
2026-08-31 22:58:15 +0200

    Add log to object creation

--- src/RedUI/Object/Create.cpp
+++ src/RedUI/Object/Create.cpp
@@ -1,10 +1,19 @@
 #include "RedUI/Object/Create.h"
+
+#include "RedUI/Debug.h"
 #include "RedUI/Object/UIObject.h"
 
 using namespace RedUI::Object;
 
 void	RedUI::Object::ParentNewUIObject(UIObjectOwner obj, UIObject *newParent)
 {
+	Debug::DLogger->Write(std::format(
+		"New UIObject created: {:p} ({}), parent: {:p} ({})",
+		static_cast<void *>(obj.get()),
+		obj->Name(),
+		static_cast<void *>(newParent),
+		newParent ? newParent->Name() : "nullptr"
+	).c_str());
 	obj->Parent = newParent;
 	(newParent == nullptr ? UIObject::RootObjects : newParent->GetChildren()).push_back(std::move(obj));
 }
