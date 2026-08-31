868a144c2658ecb9dabfe8e6e5fee999f3791df4
worse <oskar.rabo@gmail.com>
2026-09-01 00:42:59 +0200

    Add Log function

--- include/RedUI/Debug.h
+++ include/RedUI/Debug.h
@@ -4,4 +4,6 @@
 namespace RedUI::Debug
 {
 	extern IO::Logger	*DLogger;
+	void				Log(const char *str, LL logLevel = LL::Info, bool prependDatetime = true, bool appendNewline = true);
+	void				Log(const std::string &str, LL logLevel = LL::Info, bool prependDatetime = true, bool appendNewline = true);
 }
--- src/RedUI/Debug.cpp
+++ src/RedUI/Debug.cpp
@@ -3,3 +3,13 @@
 using namespace RedUI;
 
 IO::Logger *Debug::DLogger = nullptr;
+
+void Debug::Log(const char *str, const LL logLevel, const bool prependDatetime, const bool appendNewline)
+{
+	DLogger->Write(str, logLevel, prependDatetime, appendNewline);
+}
+
+void Debug::Log(const std::string &str, const LL logLevel, const bool prependDatetime, const bool appendNewline)
+{
+	DLogger->Write(str, logLevel, prependDatetime, appendNewline);
+}
