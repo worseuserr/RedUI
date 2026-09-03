45960ae165c42ab8c7e5199bebbe48a2bc15ea11
worse <oskar.rabo@gmail.com>
2026-09-03 02:03:50 +0200

    Add Error

--- src/RedUI/Debug.cpp
+++ src/RedUI/Debug.cpp
@@ -1,4 +1,7 @@
 #include "RedUI/Debug.h"
+#include "RedUI/Runtime.h"
+#include <dbghelp.h>
+#include <format>
 
 using namespace RedUI;
 
@@ -12,4 +15,24 @@ void Debug::Log(const char *str, const LL logLevel, const bool prependDatetime, 
 void Debug::Log(const std::string &str, const LL logLevel, const bool prependDatetime, const bool appendNewline)
 {
 	DLogger->Write(str, logLevel, prependDatetime, appendNewline);
+}
+
+void Debug::Error(const char *message, const bool fatal)
+{
+	std::string	str = std::format("{}rror! Message: \"{}\".{}", fatal ? "Fatal e" : "E", message, fatal ? " RedUI shutting down." : "");
+
+	DLogger->Write(str, LL::Error);
+	MessageBox(
+		nullptr,
+		str.c_str(),
+		"RedUI error!",
+		MB_ICONERROR | MB_OK
+	);
+	if (fatal)
+		Runtime::UpdateLoopEnabled = false;
+}
+
+void Debug::Error(const std::string &message, const bool fatal)
+{
+	Error(message.c_str(), fatal);
 }
